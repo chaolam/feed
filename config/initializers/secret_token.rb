4f909e64868b363f691391311c0e21d456872fa3
Chao Lam <chao@nextsmallthings.com>
2012-02-27 16:51:07 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feed::Application.config.secret_token = '511a998267306ba69527147d8b875bb4aac03598597be873de61efb10df939734fa971f0dc240645a2b249464b9f1f6f77ff12b10ef5d06e4a616334dba673ba'
