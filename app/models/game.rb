42fc1aa9ef210b9a0b1363d76d247297d9ace0a2
Chao Lam <chao@nextsmallthings.com>
2012-09-06 11:27:45 -0700

    game discovery and update now takes user access token

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,12 @@
 class Game < ActiveRecord::Base
   has_many :mygames, :dependent=>:destroy
 
-  def self.get_by_app_id(app_id)
+  def self.get_by_app_id(app_id, access_token=nil)
     game = self.find_by_app_id(app_id)
     return game if game
-    graph = Koala::Facebook::API.new
+    graph = Koala::Facebook::API.new(access_token)
     result = graph.api("/#{app_id}")
+    return nil unless result
     raise "no logo_url" if result['logo_url'].blank?
     game = Game.new(:app_id=>app_id)
     result.delete('id')
